df3160b4a547ace3abe483e1f76524e936fbac53
Henrique <hiquesc_@hotmail.com>
2020-04-06 19:47:25 -0300

    Gravando ações no banco de dados

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -4,10 +4,10 @@ module.exports.jogo = function(application, req, res) {
         return;
     }
 
-    var comando_invalido = 'N';
+    var msg = '';
     
-    if (req.query.comando_invalido == 'S') {
-        comando_invalido = 'S';
+    if (req.query.msg != '') {
+        msg = req.query.msg;
     }
     
     var usuario = req.session.usuario;
@@ -16,7 +16,7 @@ module.exports.jogo = function(application, req, res) {
     var connection = application.config.dbConnection;
     var JogoDAO = new application.app.models.JogoDAO(connection);
 
-    JogoDAO.iniciaJogo(res, usuario, casa, comando_invalido);
+    JogoDAO.iniciaJogo(res, usuario, casa, msg);
 }
 
 module.exports.sair = function(application, req, res) {
@@ -48,7 +48,7 @@ module.exports.ordenar_acao__sudito = function(application, req, res) {
         res.send('Usuário precisa se autenticar!!!');
         return;
     }
-    
+
     var dadosForm = req.body;
 
     req.assert('acao', 'Ação deve ser informada').notEmpty();
@@ -57,9 +57,15 @@ module.exports.ordenar_acao__sudito = function(application, req, res) {
     var erros = req.validationErrors();
 
     if (erros) {
-        res.redirect('jogo?comando_invalido=S');
+        res.redirect('jogo?msg=A');
         return;
     }
 
-    res.send('Tudo Ok!');
+    var connection = application.config.dbConnection;
+    var JogoDAO = new application.app.models.JogoDAO(connection);
+
+    dadosForm.usuario = req.session.usuario;
+    JogoDAO.acao(dadosForm);
+
+    res.redirect('jogo?msg=B');
 }
